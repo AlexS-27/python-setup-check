eba3057938a128708015d50a3be16ea304b64c49
Alex Kamano <pk86onf@eduvaud.ch>
2024-11-15 12:08:28 +0100

    refactor

--- src/calculatrice 2.0.py
+++ src/calculatrice 2.0.py
@@ -1,36 +1,35 @@
-from Calculatrice import chiffre1, chiffre2
+from variable import addition,subtraction,multiplication,division
 
-question = input("Entrez le calcul a effectuer (par exemple 1 + 1) : ")
+def ask_number(message):
+    while True:
+        try:
+            chiffre = float(input(message))
+            return chiffre
+        except ValueError:
+            print("Erreur : Veuillez entrez un nombre")
 
-try:
-    chiffre1_str, symbole, chiffre2_str = [element.strip() for element in question.split()]
+chiffre1=ask_number("Donnez-moi un premier chiffre : ")
+chiffre2=ask_number("Donnez-moi un deuxième chiffre : ")
+symbole= input("Dites moi si vous désirez ; une addition, une soustraction, une multiplication ou une division : " )
 
-    chiffre1 = float(chiffre1_str)
-    chiffre2 = float(chiffre2_str)
+liste_symbole=["addition", "soustraction", "multiplication", "division", "+", "-", "*", "/"]
 
-    liste_symbole=["addition", "soustraction", "multiplication", "division", "+", "-", "*", "/"]
+while symbole not in liste_symbole:
+    print("Il me faut le nom ou le symbole de la fonction")
+    symbole=input("Dites moi si vous désirez ; une addition, une soustraction, une multiplication ou une division : " )
+    if symbole in liste_symbole:
+        break
 
-    while symbole not in liste_symbole:
-        print("Il me faut le nom ou le symbole de la fonction")
-        symbole=input("Dites moi si vous désirez ; une addition, une soustraction, une multiplication ou une division : " )
-        if symbole in liste_symbole:
-            break
-
-    if symbole == "addition" or symbole == "+":
-        result=(chiffre1 + chiffre2)
-    elif symbole == "soustraction" or symbole == "-":
-        result=(chiffre1 - chiffre2)
-    elif symbole == "multiplication" or symbole == "*":
-        result=(chiffre1 * chiffre2)
-    elif symbole == "division" or symbole == "/":
-        if chiffre2 != 0:
-            result=(chiffre1 / chiffre2)
-        else:
-            print("La division par 0 est impossible")
+if symbole == "addition" or symbole == "+":
+    result=addition(chiffre1,chiffre2)
+elif symbole == "soustraction" or symbole == "-":
+    result=subtraction(chiffre1,chiffre2)
+elif symbole == "multiplication" or symbole == "*":
+    result=multiplication(chiffre1,chiffre2)
+elif symbole == "division" or symbole == "/":
+    if chiffre2 != 0:
+        result=division(chiffre1,chiffre2)
+    else:
+        print("La division par 0 est impossible")
 
     print(f"Le résultat est {result}")
-
-except ValueError:
-    print("Erreur : Veuillez mettre les deux nombres et l'opération correctement, séparé d'un espace")
-except IndexError:
-    print("Erreur : Faites attention à se que les trois élément soit séparé d'un espace")
